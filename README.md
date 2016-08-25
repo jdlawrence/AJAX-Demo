824ebad9090da602877c38728ea8556a797d1405
jdlawrence <jamil.lawrence@gmail.com>
2016-08-25 14:55:53 -0700

    Added bower.json and jquery

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AJAX-Demo
-A sandbox environment to play around with jQuery's AJAX request
+A sandbox environment to play around with jQuery's AJAX request.
+
+To get started, type "bower install" and open "index.html" in your browser.
+
+
--- ajax.js
+++ ajax.js
@@ -1,4 +1,26 @@
 console.log('hi');
+
 $(document).ready(function() {
   console.log('inside jquery: ');
+
+  $.ajax({
+    url: 'https://www.reddit.com/.json',
+    // url: 'http://www.setgetgo.com/randomword/get.php',
+    type: 'GET',
+    success: function(data) {
+      console.log('data: ', data);
+    },
+    error: function(data, textStatus, error) {
+      console.log('error: ', data, 'data', textStatus, 'error', error);
+    }
+
+  });
+
+  // .done(function(data) {
+  //   console.log('data', data);
+  // })
+  // .fail(function(jqXHR, textStatus){
+  //   console.log('textStatus: ', textStatus, jqXHR);
+  // });
+
 });
